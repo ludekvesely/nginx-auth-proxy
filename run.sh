7b447f2a28338a057087951424f0951a7fd85a8f
Luděk Veselý <ludek.vesely@email.com>
2015-12-07 13:37:45 +0100

    added UPSTREAM_ADDRESS

--- run.sh
+++ run.sh
@@ -12,6 +12,7 @@ echo "daemon off;" >> /etc/nginx/nginx.conf
 # replace variables at runtime
 sed -i "s/SERVER_NAME/${SERVER_NAME}/g" /etc/nginx/conf.d/proxy.conf
 sed -i "s/UPSTREAM_PORT/${UPSTREAM_PORT}/g" /etc/nginx/conf.d/proxy.conf
+sed -i "s/UPSTREAM_ADDRESS/${UPSTREAM_ADDRESS}/g" /etc/nginx/conf.d/proxy.conf
 
 # start the thing
 /usr/sbin/nginx
